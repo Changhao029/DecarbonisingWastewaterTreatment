34f0bef17ed6a2e1a9d8e4d0df621e78a9237bfb
ChanghaoLiu <22924454@student.uwa.edu.au>
2022-10-10 13:45:18 +0800

    improve the test

--- app/tests.py
+++ app/tests.py
@@ -8,29 +8,29 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 django.setup()
 
 
-# class ModelTest(TestCase):
-#     def setUp(self):
-#         SensorData.objects.create(sensor_datetime='2021-11-30 00:00:30',
-#                                   rainfall=None,
-#                                   rainfall_quality=None,
-#                                   temperature=None,
-#                                   temperature_quality=None,
-#                                   humidity=None,
-#                                   humidity_quality=None,
-#                                   wind_direction=None,
-#                                   wind_direction_quality=None,
-#                                   wind_speed=None,
-#                                   wind_speed_quality=None,
-#                                   pressure=None,
-#                                   pressure_quality=None,
-#                                   solar_radiation=None,
-#                                   solar_radiation_quality=None,
-#                                   station="231825A",
-#                                   )
-#
-#     def test_query(self):
-#         result = SensorData.objects.filter(station="231825A").first()
-#         self.assertEqual(result.station, "231825A")
+class ModelTest(TestCase):
+    def setUp(self):
+        SensorData.objects.create(sensor_datetime='2021-11-30 00:00:30',
+                                  rainfall=None,
+                                  rainfall_quality=None,
+                                  temperature=None,
+                                  temperature_quality=None,
+                                  humidity=None,
+                                  humidity_quality=None,
+                                  wind_direction=None,
+                                  wind_direction_quality=None,
+                                  wind_speed=None,
+                                  wind_speed_quality=None,
+                                  pressure=None,
+                                  pressure_quality=None,
+                                  solar_radiation=None,
+                                  solar_radiation_quality=None,
+                                  station="231825A",
+                                  )
+
+    def test_query(self):
+        result = SensorData.objects.filter(station="231825A").first()
+        self.assertEqual(result.station, "231825A")
 
 
 class DataTableTest(TestCase):
